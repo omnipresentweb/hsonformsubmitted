59aa741b23e9e7eee7e49dacc270e583c217bb0a
omnipresentweb <omnipresentweb@users.noreply.github.com>
2023-10-16 21:30:31 -0400

    Create hsonformsubmitted.js

---
+++ hsonformsubmitted.js
@@ -0,0 +1,63 @@
+// Initialize analytics SDKs
+const analytics = // initialize analytics SDK
+const heap = // initialize Heap SDK 
+const mutiny = // initialize Mutiny SDK
+
+// Declare ChiliPiper params
+const cpTenantDomain = 'example';
+const cpRouterName = 'contact'; 
+
+// Define a constant to specify which forms should trigger Chili Piper
+const chiliPiperForms = ["a077eb7b-965f-4716-9c26-b4248ad50743"];
+
+// Reusable function to get form data
+function getFormData(form) {
+  return Object.fromEntries(
+    Object.entries(form).map(([key, field]) => {
+      const value = Array.isArray(field.value)
+        ? field.value.toString().replaceAll(",", ";")
+        : field.value;
+      return [field.name, value];
+    })
+  );
+}
+
+// Reusable function to track conversions
+function trackConversion(name) {
+  // Mutiny
+  mutiny.client.trackConversion({
+    name
+  });
+
+  // Dreamdata
+  const email = getFormData(form).email;
+  if (email) {
+    analytics.identify(null, {email});
+    analytics.track(name);
+  }
+
+  // Heap
+  heap.track(name, {email});
+}
+
+// Reusable function for onFormSubmitted
+function onFormSubmitted(form, formId, conversionName) {
+  // Track conversions
+  trackConversion(conversionName);
+
+  // Conditionally trigger Chili Piper
+  if (chiliPiperForms.includes(formId)) {
+    const formData = getFormData(form);
+    ChiliPiper.submit(cpTenantDomain, cpRouterName, {
+      map: true,
+      lead: formData
+    });
+  }
+
+  // Google Tag Manager
+  window.dataLayer.push({
+    event: 'hubspot-form-submit',
+    'hs-form-guid': formId,
+    conversionName
+  });
+}
